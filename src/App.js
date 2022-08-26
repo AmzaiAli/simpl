514988b73a1ae120d80ae8f508c83fa3cf7b82fa
AmzaiAli <91273606+AmzaiAli@users.noreply.github.com>
2022-08-26 15:19:28 +0200

    Project Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import BarChart from './charts/BarChart';
 
 function App() {
+  const arr1 = [10, 15, 20, 35,40,45]
+  const arr2 = [51, 48, 34, 25, 15, 33]
+  const [data, setData] = useState(arr1);
+
+
+  const handlechange = (e) => {
+    const { value } = e.target;
+    if(value == 1){
+      setData(arr1);
+
+    }
+    else{
+      setData(arr2);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div  class="grid jsutify-items-center justify-center py-6 ">
+          <h2 class="text-center">Graphis with React and D3JS</h2>
+          <h3 class="text-center ">Pleas select data from witch array do you want to see</h3>
+          <select class="border-2" onChange={handlechange}>
+          <option selected value='1'>Arr1</option>
+          <option value='2'>Arr2</option>
+          </select>
+          <BarChart width={600} height={400} data={data} />
+      </div>
   );
 }
 
